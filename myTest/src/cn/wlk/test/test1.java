29963932eb372ec4bf49302e1cde391d0f691012
Bruce <1161846132@qq.com>
2020-10-02 23:33:14 +0800

    第一次提交代码

---
+++ myTest/src/cn/wlk/test/test1.java
@@ -0,0 +1,10 @@
+package cn.wlk.test;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("Hello my git");
+        System.out.println("Hello my git");
+        System.out.println("Hello my git");
+        System.out.println("Hello my git");
+    }
+}
